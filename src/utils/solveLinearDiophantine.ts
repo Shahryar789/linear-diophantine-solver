c9fa4e9de391a6a3a0f369a62c9f7bc1eff7f7e4
Shahryar789 <224642818+Shahryar789@users.noreply.github.com>
2025-08-16 20:59:54 -0400

    Established extendedGCD function Returns the [gcd, x, y] such that ax + by = gcd

--- src/utils/solveLinearDiophantine.ts
+++ src/utils/solveLinearDiophantine.ts
@@ -1,3 +1,19 @@
 //Solves ax + by = c
 //Incorporates the Extended Euclidean Algorithm
 
+//Returns the [gcd, x, y] such that ax + by = gcd
+function extendedGCD(a: number, b: number): [number, number, number]{
+    //Base case: gcd(a,0) = |a|, coefficents are (1, 0)
+    if (b === 0){
+        return [a, 1, 0];
+    }
+    //Recursion
+    const [gcd, x1, y1] = extendedGCD(b, a%b);
+
+    //Update x and y as per the result of the recursion
+    const x = y1;
+    const y = x1 - Math.floor(a/b) + y1;
+
+    return [gcd, x, y];
+}
+
