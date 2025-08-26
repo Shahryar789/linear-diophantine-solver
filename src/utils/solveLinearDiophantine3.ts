2043c8af603b6477f3039f3690d53b42decab989
Shahryar789 <224642818+Shahryar789@users.noreply.github.com>
2025-08-25 23:34:10 -0400

    Incorporated function which provides gcd for 3... numbers recursively (Extended Euclidean Algorithm)

--- src/utils/solveLinearDiophantine3.ts
+++ src/utils/solveLinearDiophantine3.ts
@@ -1,8 +1,18 @@
+//Solves ax + by + cz = d
+//Incorporates the Extended Euclidean Algorithm
+
 import{extendedGCD} from './solveLinearDiophantine';
 
+//Structures result 
 export interface Diophantine3Result {
     gcd: number,
     message: string,
     particular: {x: number, y: number, z: number} | null;
     step: {dx: [number, number]; dy: [number, number]; dz: [number, number] } | null;
+}
+
+//Returns gcd for 3 numbers recursively
+function gcd3(a: number, b: number, c: number): number {
+    const gcd2 = (x: number, y: number): number => (y === 0 ? Math.abs(x) : gcd2(y, x % y));
+    return gcd2(gcd2(a, b), c);
 }
