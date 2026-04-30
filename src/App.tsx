1410989cd845e33a53a3e251d6734ee2eae0ed46
Shahryar789 <224642818+Shahryar789@users.noreply.github.com>
2026-04-30 00:59:36 -0400

    Added temporary tests for n-variable solver

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import InputForm from './components/InputForm';
+import { solveLinearDiophantineN } from "./utils/solveLinearDiophantineN";
 
 //Main application
 //Renders input form and displays results
 
 function App(){
+  console.log(solveLinearDiophantineN([6, 9], 30));
+  console.log(solveLinearDiophantineN([6, 9, 15], 30));
+  console.log(solveLinearDiophantineN([4, 6, 8], 7));
   return(
     <div style={{padding: '20px', maxWidth: '600px', margin: '0 auto'}}>
       <h1>Linear Diophantine Equation Solver</h1>
